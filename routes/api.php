88d3f5ecdac8b4eb86e4bf74b6e7098ad788fa33
fendishatech <liyukibir@gmail.com>
2023-09-09 15:33:43 -0700

    api routes, model, migration, controller setup

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ProductsController::class)->group(function () {
+    Route::get('products', 'index');
+    Route::get('products/{id}', 'show');
+    Route::post('products', 'store');
+    Route::patch('products/{id}', 'update');
+    Route::delete('products/{id}', 'destroy');
 });
